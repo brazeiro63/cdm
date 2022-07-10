46236e7b51fd68ba5ac93e720f270a732583f304
brazeiro <brazeiro63@gmail.com>
2022-07-10 10:50:46 -0300

    Carousel implementdo completamente

--- src/components/main/Carousel.jsx
+++ src/components/main/Carousel.jsx
@@ -10,59 +10,71 @@ export default function Carousel() {
 
     return (
 
-        <div id="myCarousel" className="carousel slide" data-ride="carousel">
+        <div className='presentation'>
+            <div id="myCarousel" className="carousel slide slide--show" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
 
-            <ol className="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-                <li data-target="#myCarousel" data-slide-to="3"></li>
-            </ol>
+                <div className="carousel-inner">
+                    <div className="item active">
+                        <img className='img--carousel' src={image1} alt="Morro de São Paulo" />
+                        <div className="carousel-caption">
+                            <h3>Morro de São Paulo</h3>
+                            <p>LA is always so much fun!</p>
+                        </div>
+                    </div>
 
-            <div className="carousel-inner">
-                <div className="item active">
-                    <img className='img--carousel' src={image1} alt="Morro de São Paulo" />
-                    <div className="carousel-caption">
-                        <h3>Morro de São Paulo</h3>
-                        <p>LA is always so much fun!</p>
+                    <div className="item">
+                        <img className='img--carousel' src={image2} alt="Morro de São Paulo" />
+                        <div className="carousel-caption">
+                            <h3>Morro de São Paulo</h3>
+                            <p>Thank you, Chicago!</p>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <img className='img--carousel' src={image3} alt="Morro de São Paulo" />
+                        <div className="carousel-caption">
+                            <h3>Morro de São Paulo</h3>
+                            <p>We love the Big Apple!</p>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <img className='img--carousel' src={image4} alt="Morro de São Paulo" />
+                        <div className="carousel-caption">
+                            <h3>Morro de São Paulo</h3>
+                            <p>We love the Big Apple!</p>
+                        </div>
                     </div>
                 </div>
 
-                <div className="item">
-                    <img className='img--carousel' src={image2} alt="Morro de São Paulo" />
-                    <div className="carousel-caption">
-                        <h3>Morro de São Paulo</h3>
-                        <p>Thank you, Chicago!</p>
-                    </div>
-                </div>
-
-                <div className="item">
-                    <img className='img--carousel' src={image3} alt="Morro de São Paulo" />
-                    <div className="carousel-caption">
-                        <h3>Morro de São Paulo</h3>
-                        <p>We love the Big Apple!</p>
-                    </div>
-                </div>
-
-                <div className="item">
-                    <img className='img--carousel' src={image4} alt="Morro de São Paulo" />
-                    <div className="carousel-caption">
-                        <h3>Morro de São Paulo</h3>
-                        <p>We love the Big Apple!</p>
-                    </div>
-                </div>
-
+                <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span className="glyphicon glyphicon-chevron-left"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span className="glyphicon glyphicon-chevron-right"></span>
+                    <span className="sr-only">Next</span>
+                </a>
             </div>
-
-            <a className="left carousel-control" href="#myCarousel" data-slide="prev">
-                <span className="glyphicon glyphicon-chevron-left"></span>
-                <span className="sr-only">Previous</span>
-            </a>
-            <a className="right carousel-control" href="#myCarousel" data-slide="next">
-                <span className="glyphicon glyphicon-chevron-right"></span>
-                <span className="sr-only">Next</span>
-            </a>
+            <div className='presentation--text'>
+                <div className='presentation--text--title'>
+                    <p>Casas de Margarida</p>
+                </div>
+                <div className='presentation--text--body'>
+                    <p>Casas de Margarida tem o melhor lugar para ficar enquanto você
+                        aproveita suas férias. São imóveis bem localizados, nas melhores
+                        praias do Brasil, com todo o conforto e praticidade para que suas
+                        férias sejam um período de descanso inesquecível.</p>
+                </div>
+            </div>
         </div>
+
     )
 }
 
